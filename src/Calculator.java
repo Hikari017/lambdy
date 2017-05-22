3aee683b5d1ebfccb86da39acff6a4734e8ec2e8
justyna luszczak <justynasylwia017@gmail.com>
2017-05-22 19:13:04 +0200

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Justyna on 22.05.2017.
+ */
+public class Calculator {
+    public static void main(String[] args) {
+        IntegerMath dodawanie =  (a,b) -> a+b; //{return a+b;}
+        IntegerMath odejmowanie =  (a,b) -> a-b; //{return a-b;}
+        System.out.println(additionBinary(5,10, (a, b) -> a+b)); //15
+        System.out.println(additionBinary(20,10, (a, b) -> a-b));//10
+
+        //System.out.println(additionBinary(5,10,dodawanie)); //15
+       // System.out.println(additionBinary(20,10, odejmowanie));//10
+
+    }
+    public static int additionBinary( int a, int b, IntegerMath math){
+        return math.addition(a, b);
+    }
+    // private int addition( int a, int b){
+    //return a+b;
+}
---
+++ src/IntegerMath.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Justyna on 22.05.2017.
+ */
+@FunctionalInterface
+public interface IntegerMath {
+    //nazwa metody moze byc dowolna
+
+    int addition(int a, int b);
+
+}
